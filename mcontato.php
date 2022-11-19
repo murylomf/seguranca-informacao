b3e4cab35e608e8b3f36b22e82bf2e6447ee8e60
ifsp <ifsp@debian-BULLSEYE-live-builder-AMD64>
2022-11-18 21:17:27 -0300

    feat: add list

--- mcontato.php
+++ mcontato.php
@@ -24,6 +24,7 @@
             $conec->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $sth=$conec->prepare("SELECT * FROM contato");
             $sth->execute();
+            print "<h2 align='center'>Listagem de Contatos</h2>";
             print "<table align='center' border='1'> <tr><td>Id</td><td>Nome</td><td>Email</td></tr>";
             if($sth->rowCount()==0)
             {
@@ -48,40 +49,4 @@
         exit;
     }
   exit;
-    
-
-    //Seleção para alteração de Usuário
-    if($op=="au")
-    {
-        $conec=conec::conecta_mysql ("localhost", "root", "", "contatos");
-        try 
-        { 
-            $conec->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            print "<h2>Alteração de usuário</h2>";
-            $sth=$conec->prepare("SELECT * FROM contato WHERE cat <> '00'",
-            array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
-            $sth->execute();
-            if($sth->rowCount()==0)
-            {
-                print "Nenhum usuário para alterar <br><a href='sistema.php'>Voltar</a>";
-                exit;
-            }
-            $linha = $sth->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_FIRST);
-            print "<form method='post' action='mcontato.php?op=aeu'>";
-            print "<SELECT name='contato'>";
-            do
-            {
-                $ous= new contato($linha[0],"", $linha[2], $linha[3]);
-                print "<option value='".$ous->getcontato(). "'>".$ous->getNome();
-            } 
-            while($linha=$sth->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT));
-                print"</SELECT><INPUT type='submit' value='Editar'></form> <br><a href='sistema.php'>Voltar</a>";
-        } 
-        catch (Exception $e)
-        {   
-            print "<br>Falha: " . $e->getMessage(). "<br><a href='sistema.php'>Voltar</a>";
-            exit; 
-        }
-        exit;
-    }
 ?>
